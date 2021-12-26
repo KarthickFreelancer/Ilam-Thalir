0c9be99f97979af83fc4d6e6e2d0b6d1e0b67bff
KarthickFreelancer <95900734+KarthickFreelancer@users.noreply.github.com>
2021-12-26 14:33:43 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function dropdown(){
+    menuElement=document.getElementById('menu');
+    navIcon=document.getElementById('nav-toggle');
+    navIcon.classList.toggle('active');
+    menuElement.classList.toggle('menu-active');
+}
